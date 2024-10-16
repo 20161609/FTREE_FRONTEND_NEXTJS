210e2d854414e7c93c88dddbd70202e96da8a325
20161609 <dksshwns@naver.com>
2024-10-17 05:28:21 +0900

    [Refactor] On Setting Modal, 2 buttons(save, cancel) should be blocked from autumatic translation

--- src/components/ModalSettings.jsx
+++ src/components/ModalSettings.jsx
@@ -225,17 +225,17 @@ export default function ModalSettings({
                   <div className="mt-6 flex justify-end">
                     <button
                       type="button"
-                      className="mr-2 inline-flex justify-center rounded-md border border-transparent bg-gray-200 dark:bg-gray-700 px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-600"
+                      className="mr-2 inline-flex justify-center rounded-md border border-transparent bg-gray-200 dark:bg-gray-700 px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-600 notranslate"
                       onClick={closeModal}
                     >
                       Cancel
                     </button>
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 notranslate"
                       onClick={handleSave}
                     >
-                      Confirm
+                      Save
                     </button>
                   </div>
                 </div>
